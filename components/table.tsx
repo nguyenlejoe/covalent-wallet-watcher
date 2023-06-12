fe2936006421a0474b31a23f81f0941e19a03554
nguyenlejoe <joe_le.1@hotmail.com>
2023-06-12 15:07:17 -0700

    fix

--- components/table.tsx
+++ components/table.tsx
@@ -32,7 +32,7 @@ export default async function Table() {
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">Recent Users</h2>
           <p className="text-sm text-gray-500">
-            Fetched {users && users.length} users in {duration}ms
+            {/* Fetched {users && users.length} users in {duration}ms */}
           </p>
         </div>
         <RefreshButton />
